abc536cc7cf6aac27b39cad23b5ee37b229b3ac7
DavidSeoaneMachado <dseoanemachado@danielcastelao.org>
2022-11-13 17:18:47 +0100

    ejercicio 2(si) resuelto

--- toquetear_metodos/src/semana8/ejercicio2.java
+++ toquetear_metodos/src/semana8/ejercicio2.java
@@ -4,6 +4,8 @@ public class ejercicio2 {
 
     public static void Azar (int max, int min) {
 
+        int aleatorio = (int)Math.floor(Math.random()*(max-min+1)+min);  // formula para generar valores entre los max y min que le damos
+        System.out.println(aleatorio);
 
     }
 }
