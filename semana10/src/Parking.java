f288f4c9ed93891c460e87cc1b384518607797ff
DavidSeoaneMachado <dseoanemachado@danielcastelao.org>
2022-12-10 17:48:22 +0100

    Casi todo listo

--- semana10/src/Parking.java
+++ semana10/src/Parking.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Parking {
-    public int tamaño_parking;
-    public Vehiculo[] parking;
+    private int tamaño_parking;
+    private Vehiculo[] parking;
     Scanner cubrir = new Scanner(System.in);
 
     public Parking(int tamaño_parking) {
@@ -117,17 +117,69 @@ public class Parking {
     }
 
     public void retirar_vehiculos(Vehiculo[] arra) {
-        System.out.println("¿Como quieres retirar el vehiculo? \n1 Por plaza \n2 Por matricula \3 De manera aleatoria");
+        System.out.println("¿Como quieres retirar el vehiculo? \n1 Por plaza \n2 Por matricula \n3 De manera aleatoria");
         int modo_retirada = cubrir.nextInt();
         switch (modo_retirada) {
 
             case 1:
                 System.out.println("¿Que plaza quieres dejar vacia?");
                 int plaza_vacia = cubrir.nextInt();
+                String var_para_borrar = "";
                 for (int i = 0; i < arra.length; i++) {
+                    var_para_borrar = arra[plaza_vacia - 1].getMatricula();
+                    if (arra[i].getMatricula() == var_para_borrar) {
 
+                        if (arra[i].getMatricula() == arra[i + 1].getMatricula()) {
+                            System.out.println("Se ha retirado un vehiculo de las plazas " + (i + 1) + " y " + (i + 2) + " con matricula: " + arra[i].getMatricula());
+                            arra[i].setTipo("retirado, ");
+                            arra[i].setMatricula("-");
+                            i++;
+                        } else {
+                            System.out.println("Se ha retirado un vehiculo de la plaza " + (i + 1) + "con matricula: " + arra[i].getMatricula());
+                            arra[i].setTipo("retirado, ");
+                            arra[i].setMatricula("-");
+                        }
+                    }
+                }
+            case 2:
+                System.out.println("¿Que matricula (con su correspondiente vehiculo) quieres retirar del parking?");
+                String matricula_a_borrar = "";
+                for (int i = 0; i < arra.length; i++) {
+                    if (arra[i].getMatricula() == matricula_a_borrar) {
+                        if (arra[i].getMatricula() == arra[i + 1].getMatricula()) {
+                            System.out.println("Se ha retirado un vehiculo de las plazas " + (i + 1) + " y " + (i + 2) + " con matricula: " + arra[i].getMatricula());
+                            arra[i].setTipo("retirado, ");
+                            arra[i].setMatricula("-");
+                            i++;
+                        } else {
+                            System.out.println("Se ha retirado un vehiculo de la plaza " + (i + 1) + "con matricula: " + arra[i].getMatricula());
+                            arra[i].setTipo("retirado, ");
+                            arra[i].setMatricula("-");
+                        }
+                    }
+                }
+            case 3:
+                int aleat_para_retirar = (int) Math.floor(Math.random() * ((arra.length) - 1 + 1) + 1);
+                System.out.println(aleat_para_retirar);
+                for (int i = 1; i < arra.length; i++) {
+                    if (arra[aleat_para_retirar].getMatricula() != "-") {
+                        if (arra[i].getMatricula() == arra[aleat_para_retirar].getMatricula()) {
+                            if (arra[i+1].getMatricula() == arra[aleat_para_retirar].getMatricula()) {
+                                System.out.println("Se ha retirado un vehiculo de las plazas " + (aleat_para_retirar) + " y " + (aleat_para_retirar + 1) + " con matricula: " + arra[aleat_para_retirar].getMatricula());
+                                arra[aleat_para_retirar].setTipo("retirado, ");
+                                arra[aleat_para_retirar].setMatricula("-");
+                                arra[aleat_para_retirar+1].setTipo("retirado, ");
+                                arra[aleat_para_retirar+1].setMatricula("-");
+                                i++;
+                            } else {
+                                System.out.println("Se ha retirado un vehiculo de la plaza " + (aleat_para_retirar) + " con matricula: " + arra[aleat_para_retirar].getMatricula());
+                                arra[aleat_para_retirar].setTipo("retirado, ");
+                                arra[aleat_para_retirar].setMatricula("-");
+                            }
+                        }
+                    }
                 }
         }
-        }
     }
+}
 
